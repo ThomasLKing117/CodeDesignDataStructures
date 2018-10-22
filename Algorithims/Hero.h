721043c9fc184730413cd1efd09ab0e43a99fc6a
ThomasLKing117 <king.thomas.l.117@gmail.com>
2018-10-22 16:01:43 -0500

    ?

--- Algorithims/Hero.h
+++ Algorithims/Hero.h
@@ -1,7 +1,22 @@
 #pragma once
+#include <cstdlib>
+#include <time.h>
 
 class Hero
 {
 private:
 	int mHealth;
+public:
+	Hero();
+	int GetHealth();
 };
+
+Hero::Hero()
+{
+	mHealth = rand() % 100 + 1;
+}
+
+int Hero::GetHealth()
+{
+	return mHealth;
+}
--- Algorithims/Main.cpp
+++ Algorithims/Main.cpp
@@ -2,5 +2,13 @@
 
 int main()
 {
+	srand(time(NULL));
 
+	for (int i = 0; i < 2; i++)
+	{
+		for (int j = 0; j < 2; i++)
+		{
+
+		}
+	}
 }
