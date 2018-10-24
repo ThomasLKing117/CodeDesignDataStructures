6295840b185103a631f2546a428204d0262c31c5
ThomasLKing117 <king.thomas.l.117@gmail.com>
2018-10-24 16:22:26 -0500

    Finished test case 1

--- LinkeList/Main.cpp
+++ LinkeList/Main.cpp
@@ -6,16 +6,16 @@ int main()
 
 	//Test Case 1
 	{
-		//Expected: Create new nodetType
-		//Result: A new nodeType N3 created
+		//Expected: Create new nodetType of type int called N1
+		//Result: A new nodeType of type int is created
 		nodeType<int>* N1 = new nodeType<int>;
 
-		//Expected: Create new nodetType
-		//Result: A new nodeType N3 created
+		//Expected: Create new nodetType of type int called N2
+		//Result: A new nodeType of type int is created
 		nodeType<int>* N2 = new nodeType<int>;
 
-		//Expected: Create new nodetType
-		//Result: A new nodeType N3 created
+		//Expected: Create new nodetType of type int called N3
+		//Result: A new nodeType of type int is created called N1
 		nodeType<int>* N3 = new nodeType<int>;
 
 		//Expected: Give N1->info the value of 1
@@ -30,90 +30,244 @@ int main()
 		//Result: N3->info is assigned the value of 3
 		N3->info = 3;
 
+		//Expected: Connect N1 to N2
+		//Result: N1 is connected to N2
 		N1->next = N2;
+
+		//Expected: Connect N2 to N3
+		//Result: N2 is connected to N3
 		N2->next = N3;
+
+		//Expected: Connect N3 to nullptr
+		//Result: N3 is connected to nullptr
 		N3->next = nullptr;
 
+		//Expected: Create a Iterator of type int called Iterate
+		//Result: A new Iterator of type int is created
 		Iterator<int> Iterate(N1);
+
+		//Expected: Create a for loop to increment the Iterator type for each node created
+		//Result:  The for loop should run through 3 times for each nodeType
 		for (int i = 0; i < 3; i++)
 		{
+			//Expected: Increment the Iterator Type
+			//Result: The Iterator should go through each node
 			++Iterate;
 		}
 	}
 
 	//Test Case 2
 	{
+		//Expected:
+		//Result:
 		UnorderedLinkedList<int> FirstList;
+
+		//Expected:
+		//Result:
 		FirstList.initalizeList();
 
+		//Expected:
+		//Result:
 		FirstList.insertFirst(1);
+
+		//Expected:
+		//Result:
 		FirstList.insertLast(2);
+
+		//Expected:
+		//Result:
 		FirstList.insertFirst(3);
+
+		//Expected:
+		//Result:
 		FirstList.insertLast(4);
+
+		//Expected:
+		//Result:
 		FirstList.insertFirst(5);
 
+		//Expected:
+		//Result:
 		UnorderedLinkedList<int> SecondList;
+
+		//Expected:
+		//Result:
 		SecondList.initalizeList();
 
+		//Expected:
+		//Result:
 		SecondList.insertLast(6);
+
+		//Expected:
+		//Result:
 		SecondList.insertFirst(7);
+
+		//Expected:
+		//Result:
 		SecondList.insertLast(8);
+
+		//Expected:
+		//Result:
 		SecondList.insertFirst(9);
+
+		//Expected:
+		//Result:
 		SecondList.insertLast(10);
 
+		//Expected:
+		//Result:
 		SecondList = FirstList;
 
+		//Expected:
+		//Result:
 		SecondList.insertLast(6);
+
+		//Expected:
+		//Result:
 		SecondList.insertFirst(7);
+
+		//Expected:
+		//Result:
 		SecondList.insertLast(8);
+
+		//Expected:
+		//Result:
 		SecondList.insertFirst(9);
+
+		//Expected:
+		//Result:
 		SecondList.insertLast(10);
 
-		SecondList.isEmptyList();
-		SecondList.destroyList();
+		//Expected:
+		//Result:
 		SecondList.isEmptyList();
 
+		//Expected:
+		//Result:
+		SecondList.destroyList();
+
+		//Expected:
+		//Result:
+		SecondList.isEmptyList();
+
+		//Expected:
+		//Result:
 		UnorderedLinkedList<int> ThirdList;
+
+		//Expected:
+		//Result:
 		ThirdList.initalizeList();
 
+		//Expected:
+		//Result:
 		ThirdList = FirstList;
 
+		//Expected:
+		//Result:
 		ThirdList.insertFirst(0);
+
+		//Expected:
+		//Result:
 		ThirdList.insertLast(117);
+
+		//Expected:
+		//Result:
 		ThirdList.search(117);
+
+		//Expected:
+		//Result:
 		ThirdList.print();
 
+		//Expected:
+		//Result:
 		ThirdList.deleteNode(117);
+
+		//Expected:
+		//Result:
 		ThirdList.search(117);
+
+		//Expected:
+		//Result:
 		ThirdList.print();
 
+		//Expected:
+		//Result:
 		ThirdList.insertFirst(117);
+
+		//Expected:
+		//Result:
 		ThirdList.search(117);
+
+		//Expected:
+		//Result:
 		ThirdList.print();
 
+		//Expected:
+		//Result:
 		ThirdList.begin();
+
+		//Expected:
+		//Result:
 		ThirdList.front();
 
+		//Expected:
+		//Result:
 		ThirdList.end();
+
+		//Expected:
+		//Result:
 		ThirdList.back();
 
+		//Expected:
+		//Result:
 		ThirdList.length();
 
-
+		//Expected:
+		//Result:
 		UnorderedLinkedList<int> FourthList(ThirdList);
 		
+		//Expected:
+		//Result:
 		FourthList.search(117);
+
+		//Expected:
+		//Result:
 		FourthList.destroyList();
+
+		//Expected:
+		//Result:
 		FourthList.search(117);
+
+		//Expected:
+		//Result:
 		FourthList.insertFirst(117);
+
+		//Expected:
+		//Result:
 		FourthList.search(117);
 
+		//Expected:
+		//Result:
 		FirstList.destroyList();
+
+		//Expected:
+		//Result:
 		ThirdList.destroyList();
+
+		//Expected:
+		//Result:
 		FourthList.destroyList();
 
+		//Expected:
+		//Result:
 		FirstList.isEmptyList();
+
+		//Expected:
+		//Result:
 		ThirdList.isEmptyList();
+
+		//Expected:
+		//Result:
 		FourthList.isEmptyList();
 	}
 
