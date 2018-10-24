f0949b78ae71200af91c8729f47a616a4faa5af9
ThomasLKing117 <king.thomas.l.117@gmail.com>
2018-10-24 17:45:28 -0500

    finished with test case 2

--- LinkeList/Main.cpp
+++ LinkeList/Main.cpp
@@ -62,23 +62,23 @@ int main()
 		//Result: A new UnorderedLinkedList of type is created
 		UnorderedLinkedList<int> FirstList;
 
-		//Expected: insert 1 in the first position in the FirstList
+		//Expected: insert 1 in the first position in the list
 		//Result: 1 is now in the first position of the FirstList
 		FirstList.insertFirst(1);
 
-		//Expected: insert 2 in the last position in the FirstList
+		//Expected: insert 2 in the last position in the list
 		//Result: 2 is now in the last position of the FirstList
 		FirstList.insertLast(2);
 
-		//Expected: insert 3 in the first position in the FirstList
+		//Expected: insert 3 in the first position in the list
 		//Result: 3 is now in the first position of the FirstList
 		FirstList.insertFirst(3);
 
-		//Expected: insert 4 in the Last position in the FirstList
+		//Expected: insert 4 in the Last position in the list
 		//Result: 4 is now in the Last position of the FirstList
 		FirstList.insertLast(4);
 
-		//Expected: insert 5 in the first position in the FirstList
+		//Expected: insert 5 in the first position in the list
 		//Result: 5 is now in the first position of the Firstlist
 		FirstList.insertFirst(5);
 
@@ -86,23 +86,23 @@ int main()
 		//Result: A new UnorderedLinkedList of type is created
 		UnorderedLinkedList<int> SecondList;
 
-		//Expected: insert 6 in the Last position in the SecondList
+		//Expected: insert 6 in the Last position in the list
 		//Result: 6 is now in the Last position of the Secondlist
 		SecondList.insertLast(6);
 
-		//Expected: insert a 7 in the first position in the SecondList
+		//Expected: insert a 7 in the first position in the list
 		//Result: 7 is now in the first position of the SecondList
 		SecondList.insertFirst(7);
 
-		//Expected: insert 8 in the Last position in the SecondList
+		//Expected: insert 8 in the Last position in the list
 		//Result: 8 is now in the Last position of the SecondList
 		SecondList.insertLast(8);
 
-		//Expected: insert a 9 in the first position in the SecondList
+		//Expected: insert a 9 in the first position in the list
 		//Result: 9 is now in the first position of the SecondList
 		SecondList.insertFirst(9);
 
-		//Expected: insert 10 in the Last position in the SecondList
+		//Expected: insert 10 in the Last position in the list
 		//Result: 10 is now in the Last position of the SecondList
 		SecondList.insertLast(10);
 
@@ -110,35 +110,35 @@ int main()
 		//Result: The SecondList and FirstList are identical and can be changed seperatly
 		SecondList = FirstList;
 
-		//Expected: insert 6 in the Last position in the SecondList
+		//Expected: insert 6 in the Last position in the list
 		//Result: 6 is now in the Last position of the SecondList
 		SecondList.insertLast(6);
 
-		//Expected: insert a 7 in the first position in the SecondList
+		//Expected: insert a 7 in the first position in the list
 		//Result: 7 is now in the first position of the SecondList
 		SecondList.insertFirst(7);
 
-		//Expected: insert 8 in the Last position in the SecondList
+		//Expected: insert 8 in the Last position in the list
 		//Result: 8 is now in the Last position of the SecondList
 		SecondList.insertLast(8);
 
-		//Expected: insert a 9 in the first position in the SecondList
+		//Expected: insert a 9 in the first position in the list
 		//Result: 9 is now in the first position of the SecondList
 		SecondList.insertFirst(9);
 
-		//Expected: insert 10 in the Last position in the SecondList
+		//Expected: insert 10 in the Last position in the list
 		//Result: 10 is now in the Last position of the SecondList
 		SecondList.insertLast(10);
 
-		//Expected: The isEmptyList method checks to see if the SecondList is empty
+		//Expected: The isEmptyList method checks to see if the list is empty
 		//Result: In this case it should return false
 		SecondList.isEmptyList();
 
-		//Expected: The destroyList method deletes all the nodes in a SecondList
+		//Expected: The destroyList method deletes all the nodes in a list
 		//Result: Clears out the list
 		SecondList.destroyList();
 
-		//Expected: The isEmptyList method checks to see if the SecondList is empty
+		//Expected: The isEmptyList method checks to see if the list is empty
 		//Result: In this case it should return true
 		SecondList.isEmptyList();
 
@@ -146,120 +146,120 @@ int main()
 		//Result: A new UnorderedLinkedList of type is created
 		UnorderedLinkedList<int> ThirdList;
 
-		//Expected:
-		//Result:
+		//Expected: Have the ThirdList copy the FirstList
+		//Result: The ThirdList and FirstList are identical and can be changed seperatly
 		ThirdList = FirstList;
 
-		//Expected: insert a 1 in the first position in the list 
-		//Result: 1 is now in the first position
+		//Expected: insert a 0 in the first position in the list 
+		//Result: 0 is now in the first position of the ThirdList
 		ThirdList.insertFirst(0);
 
-		//Expected: insert 2 in the Last position in the list 
-		//Result: 2 is now in the Last position
+		//Expected: insert 117 in the Last position in the list 
+		//Result: 117 is now in the Last position of the ThirdList
 		ThirdList.insertLast(117);
 
-		//Expected:
-		//Result:
+		//Expected: The search method looks through the list for the argument passed through
+		//Result: In this case it returns true
 		ThirdList.search(117);
 
-		//Expected:
-		//Result:
+		//Expected: The print method prints the values of all the nodes in the lsit to the console
+		//Result: All the node values are visible on the console
 		ThirdList.print();
 
-		//Expected:
-		//Result:
+		//Expected: The deleteNode method seaches throught the list for the node its trying to delete then deletes that node
+		//Result: The nodeType with the value 117 is now deleted
 		ThirdList.deleteNode(117);
 
-		//Expected:
-		//Result:
+		//Expected: The search method looks through the list for the argument passed through
+		//Result: In this case it returns false
 		ThirdList.search(117);
 
-		//Expected:
-		//Result:
+		//Expected: The print method prints the values of all the nodes in the lsit to the console
+		//Result: All the node values are visible on the console
 		ThirdList.print();
 
-		//Expected: insert a 1 in the first position in the list 
-		//Result: 1 is now in the first position
+		//Expected: insert a 117 in the first position in the list 
+		//Result: 117 is now in the first position of the ThirdList
 		ThirdList.insertFirst(117);
 
-		//Expected:
-		//Result:
+		//Expected: The search method looks through the List for the argument passed through
+		//Result: In this case it returns true
 		ThirdList.search(117);
 
-		//Expected:
-		//Result:
+		//Expected: The print method prints the values of all the nodes in the lsit to the console
+		//Result: All the node values are visible on the console
 		ThirdList.print();
 
-		//Expected:
-		//Result:
+		//Expected: The begin method retruns the first node in the list
+		//Result: In this case it returns the node 117
 		ThirdList.begin();
 
-		//Expected:
-		//Result:
+		//Expected: The front method returns the info of the first node in the list
+		//Result: In this case it returns the value 117
 		ThirdList.front();
 
-		//Expected:
-		//Result:
+		//Expected: The end method retruns the last node in the list
+		//Result: In this case it returns the node 4
 		ThirdList.end();
 
-		//Expected:
-		//Result:
+		//Expected: The back method returns the info of the last node in the list
+		//Result: In this case it returns the value 4
 		ThirdList.back();
 
-		//Expected:
-		//Result:
+		//Expected: The length method tells you how many nodes are in the list by returning mCount
+		//Result: In this case it returns 7
 		ThirdList.length();
 
 		//Expected: Create a new UnorderedLinkedList type called FourthList that takes in an argument of ThirdList
 		//Result: A new UnorderedLinkedList of type is created that is a copy of the ThirdList
 		UnorderedLinkedList<int> FourthList(ThirdList);
 		
-		//Expected:
-		//Result:
+		//Expected: The search method looks through the list for the argument passed through
+		//Result: In this case it returns True
 		FourthList.search(117);
 
-		//Expected:
-		//Result:
+		//Expected: The destroyList method deletes all the nodes in a list
+		//Result: Clears out the list
 		FourthList.destroyList();
 
-		//Expected:
-		//Result:
+		//Expected: The search method looks through the list for the argument passed through
+		//Result: In this case it returns false
 		FourthList.search(117);
 
-		//Expected:
-		//Result:
+		//Expected: The initializelist method sets the first and last ptr to nullptr and mCount to 0
+		//Result: Sets the FourthList to have first and last pointing to nullptr and mCount to 0
 		FourthList.initalizeList();
 
-		//Expected: insert a 1 in the first position in the list 
-		//Result: 1 is now in the first position
+		//Expected: insert a 117 in the first position in the list 
+		//Result: 117 is now in the first position of the FourthList
 		FourthList.insertFirst(117);
 
-		//Expected:
-		//Result:
+		//Expected: The search method looks through the list for the argument passed through
+		//Result: In this case it returns True
 		FourthList.search(117);
 
-		//Expected:
-		//Result:
+		//Expected: The destroyList method deletes all the nodes in a list
+		//Result: Clears out the list
 		FirstList.destroyList();
 
-		//Expected:
-		//Result:
+		//Expected: The destroyList method deletes all the nodes in a list
+		//Result: Clears out the list
 		ThirdList.destroyList();
 
-		//Expected:
-		//Result:
+		//Expected: The destroyList method deletes all the nodes in a list
+		//Result: Clears out the list
 		FourthList.destroyList();
 
-		//Expected:
-		//Result:
+		//Expected: The isEmptyList method checks to see if the list is empty
+		//Result: In this case it should return true
 		FirstList.isEmptyList();
 
-		//Expected:
-		//Result:
+		//Expected: The isEmptyList method checks to see if the list is empty
+		//Result: In this case it should return true
 		ThirdList.isEmptyList();
 
-		//Expected:
-		//Result:
+		//Expected: The isEmptyList method checks to see if the list is empty
+		//Result: In this case it should return true
 		FourthList.isEmptyList();
 	}
 
