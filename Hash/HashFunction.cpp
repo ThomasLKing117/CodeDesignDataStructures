38a6ab5b71493eab1a8ae0f3729bf2a9447f8b2f
ThomasLKing117 <king.thomas.l.117@gmail.com>
2018-10-24 15:41:16 -0500

    ?

---
+++ Hash/HashFunction.cpp
@@ -0,0 +1,17 @@
+#include "HashFunction.h"
+
+//implementation of a basic addition hash
+namespace HashFunction
+{
+	unsigned int badHash(const char * data, unsigned int length)
+	{
+		unsigned int hash = 0;
+
+		for (unsigned int i = 0; i < length; i++)
+		{
+			hash += data[i];
+		}
+
+		return hash;
+	}
+}
---
+++ Hash/HashFunction.h
@@ -0,0 +1,11 @@
+#pragma once
+#include <functional>
+
+namespace HashFunction 
+{
+	//implementation of a basic addition hash
+	unsigned int badHash(const char* data, unsigned int length);
+
+	//my functions here
+
+}
