32545d3f6b2a378e9bfab4d8458e45e0b70e583e
ThomasLKing117 <king.thomas.l.117@gmail.com>
2018-12-03 14:09:20 -0600

    added comments

--- DynamicArrays/Array.h
+++ DynamicArrays/Array.h
@@ -1,17 +1,32 @@
 #pragma once
 
+//templated class to make dynamic array
 template <typename T>
 class DynamicArray
 {
 private:
+	//pointer for dynamic array
 	T* data;
+
+	//size of the array
 	int allocatedSpace;
+
+	//the amount of indexs' being used
 	int usedSpace;
 public:
+	//constructor for dynamic array
 	DynamicArray();
+
+	//creates an array and sets the length of the array
 	void CreateArray(int initialSize);
+
+	//inserts a type into the next index of last index used
 	void InsertLast(T);
+
+	//removes the last index
 	void DeleteLast();
+
+	//retrieves the data from the chosen index
 	T getData(T);
 };
 
@@ -23,6 +38,7 @@ DynamicArray<T>::DynamicArray()
 template<typename T>
 void DynamicArray<T>::CreateArray(int initialSize)
 {
+	//sets the array size and initializes the array
 	data = new T[initialSize];
 	allocatedSpace = initialSize;
 	usedSpace = 0;
@@ -31,18 +47,24 @@ void DynamicArray<T>::CreateArray(int initialSize)
 template<typename T>
 void DynamicArray<T>::InsertLast(T newdata)
 {
+	//checks to see if the array is full
 	if (usedSpace == allocatedSpace)
 	{
+		//extends the amount of usable space by a multiple of 2
 		allocatedSpace *= 2;
+
+		//creates another temporary Dynamic Array and sets its length to the multiplied length
 		DynamicArray<T>* test = new DynamicArray<T>[allocatedSpace];
 		test->CreateArray(allocatedSpace);
 
+		//copies the old array into the temporary array and delets the old
 		for (int i = 0; i < usedSpace; i++)
 		{
 			test->data[i] = data[i];
 		}
 		delete data;
 
+		//copies the temporary array into the new array then deltes the temporary array
 		data = new T[allocatedSpace];
 
 		for (int i = 0; i < usedSpace; i++)
@@ -51,11 +73,14 @@ void DynamicArray<T>::InsertLast(T newdata)
 		}
 		delete test;
 
+		//sets the next index to the inputed value and moves to the nex index
 		data[usedSpace] = newdata;
 		usedSpace += 1;
 
 		return;
 	}
+
+	//sets the next index to the inputed value and moves to the nex index
 	data[usedSpace] = newdata;
 	usedSpace += 1;
 }
@@ -63,11 +88,13 @@ void DynamicArray<T>::InsertLast(T newdata)
 template<typename T>
 void DynamicArray<T>::DeleteLast()
 {
+	//removes the last spot used in the array
 	usedSpace -= 1;
 }
 
 template<typename T>
 inline T DynamicArray<T>::getData(T index)
 {
+	//returns the value in the specified index
 	return data[index];
 }
--- DynamicArrays/main.cpp
+++ DynamicArrays/main.cpp
@@ -3,24 +3,52 @@
 
 int main()
 {
+	//makes dynamic array
 	DynamicArray<int> MyArray;
 
+	//creates the array with the specified space
 	MyArray.CreateArray(5);
 
+	//Insert a value in the back of the array
 	MyArray.InsertLast(1);
+
+	//Insert a value in the back of the array
 	MyArray.InsertLast(2);
+
+	//Insert a value in the back of the array
 	MyArray.InsertLast(3);
+
+	//Insert a value in the back of the array
 	MyArray.InsertLast(4);
+
+	//Insert a value in the back of the array
 	MyArray.InsertLast(5);
+
+	//Insert a value in the back of the array
 	MyArray.InsertLast(6);	
+
+	//Insert a value in the back of the array
 	MyArray.InsertLast(7);
+
+	//Insert a value in the back of the array
 	MyArray.InsertLast(8);
+
+	//Insert a value in the back of the array
 	MyArray.InsertLast(9);
+
+	//Insert a value in the back of the array
 	MyArray.InsertLast(10);
+
+	//Insert a value in the back of the array
 	MyArray.InsertLast(11);
+
+	//Insert a value in the back of the array
 	MyArray.InsertLast(12);
+
+	//deltes the value in the last used index of the array
 	MyArray.DeleteLast();
 
+	//print out the value in the 5th index of the array
 	std::cout << MyArray.getData(5) << std::endl;
 
 	return 0;
